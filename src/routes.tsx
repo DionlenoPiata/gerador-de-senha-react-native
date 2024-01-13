8048f3f48b2e54b934fc54c7f75b27a1df3176d8
DionlenoPiata <contato.dionleno@gmail.com>
2024-01-13 10:53:37 -0300

    Adicionando icones na navegação

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import {Home} from './pages/Home';
 import {Passwords} from './pages/Passwords';
 
@@ -8,8 +9,36 @@ const Tab = createBottomTabNavigator();
 export function Routes() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="home" component={Home} />
-      <Tab.Screen name="passwords" component={Passwords} />
+      <Tab.Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({focused, size, color}) => {
+            if (focused) {
+              return <Ionicons name="home" size={size} color={color} />;
+            }
+            return <Ionicons name="home-outline" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="passwords"
+        component={Passwords}
+        options={{
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({focused, size, color}) => {
+            if (focused) {
+              return <Ionicons name="lock-closed" size={size} color={color} />;
+            }
+            return (
+              <Ionicons name="lock-closed-outline" size={size} color={color} />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
